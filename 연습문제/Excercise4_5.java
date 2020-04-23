e10407005c8110c2432a0e1d013f093c9f517fda
YangTaeyoung <59782504+YangTaeyoung@users.noreply.github.com>
2020-04-24 00:35:27 +0900

    Create Excercise4_5.java

---
+++ 연습문제/Excercise4_5.java
@@ -0,0 +1,20 @@
+
+public class Excercise4_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		int j = 0;
+		while(i <= 10)
+		{
+			j = 0;
+			while(j <= i)
+			{
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
